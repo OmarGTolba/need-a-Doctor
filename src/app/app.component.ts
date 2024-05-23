f53f112af3f191c45fad48c5214cc476c1938b33
OmarGTolba <omar.739.tolba@gmail.com>
2024-05-24 00:11:28 +0300

    v1.0.1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,11 +10,13 @@ import { TranslateServices } from './translate.service';
 export class AppComponent {
   title = 'iNeedADoctor';
   direction = 'ltr';
-
+  currentSlideIndex =0
   constructor(private translate: TranslateService, private translateServices: TranslateServices) {
     translate.setDefaultLang('en');
   }
-
+  setCurrentSlideIndex(index: number) {
+    this.currentSlideIndex = index;
+  }
   ngOnInit() {
     this.translateServices.direction.subscribe(direction => {
       this.direction = direction;
--- src/app/star-rating/star-rating.component.ts
+++ src/app/star-rating/star-rating.component.ts
@@ -7,12 +7,17 @@ import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 })
 export class StarRatingComponent implements OnChanges {
   @Input() rating: number = 0;
+  @Input() starColor: string = '';
+
+
+color:any =''
   stars: boolean[] = [];
 
   ngOnChanges(changes: SimpleChanges) {
     if (changes) {
       this.updateStars();
     }
+    this.color = this.starColor
   }
 
   private updateStars() {
